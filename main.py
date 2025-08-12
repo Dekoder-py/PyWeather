a964acb4581c516e8bf4bb78c6cc1153dc114df1
Dekoder-py <dekod3r.py@gmail.com>
2025-08-12 12:49:22 +1200

    check conditions

--- main.py
+++ main.py
@@ -16,9 +16,30 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
+weather_codes = {
+    1000: "clear",
+    1100: "mostly clear",
+    1101: "partly cloudy",
+    1102: "mostly cloudy",
+}
+
 if response.status_code == 200:
     content = response.json()
     values = content["data"]["values"]
-    print(f"The temperature in {location} is {values["temperature"]}")
+    condition = weather_codes[values["weatherCode"]]
+    print(f"The temperature in {location} is {values['temperature']}")
+    print(f"It is {condition}.")
+    if condition == weather_codes[1100]:
+        print(
+            r"""
+  \   /  
+   .-.   
+― (   ) ―
+   `-’   
+  /   \  
+
+"""
+        )
+
 else:
     print(response)
