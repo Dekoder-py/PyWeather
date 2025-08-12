73fa93aea3ba4597d632e4077ea48bf2cd51a1d3
Dekoder-py <dekod3r.py@gmail.com>
2025-08-12 17:51:46 +1200

    add all weather codes

--- pyioweather/main.py
+++ pyioweather/main.py
@@ -6,10 +6,30 @@ from dotenv import load_dotenv
 from rich import print
 
 WEATHER_CODES = {
-    1000: "clear",
-    1100: "mostly clear",
-    1101: "partly cloudy",
-    1102: "mostly cloudy",
+    0: "Unknown",
+    1000: "Clear, Sunny",
+    1100: "Mostly Clear",
+    1101: "Partly Cloudy",
+    1102: "Mostly Cloudy",
+    1001: "Cloudy",
+    2000: "Fog",
+    2100: "Light Fog",
+    4000: "Drizzle",
+    4001: "Rain",
+    4200: "Light Rain",
+    4201: "Heavy Rain",
+    5000: "Snow",
+    5001: "Flurries",
+    5100: "Light Snow",
+    5101: "Heavy Snow",
+    6000: "Freezing Drizzle",
+    6001: "Freezing Rain",
+    6200: "Light Freezing Rain",
+    6201: "Heavy Freezing Rain",
+    7000: "Ice Pellets",
+    7101: "Heavy Ice Pellets",
+    7102: "Light Ice Pellets",
+    8000: "Thunderstorm",
 }
 
 
@@ -85,7 +105,7 @@ def main():
         print(f"Current temperature: {values['temperature']}\u00b0C")
 
         condition = WEATHER_CODES[values["weatherCode"]]
-        print(f"It is {condition}.")
+        print(f"Current conditions: {condition.lower()}.")
         display_weather_art(condition)
     elif response.status_code == 400:
         print("Invalid Location.")
