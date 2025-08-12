224665b6341948fd6cee932406436e01cc39a429
Dekoder-py <dekod3r.py@gmail.com>
2025-08-12 13:27:45 +1200

    add art for cloudy conditions

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ import requests
 from dotenv import load_dotenv
 from rich import print
 
+
+def display_weather_art():
+    if condition == weather_codes[1000] or condition == weather_codes[1100]:
+        print(
+            r"""[yellow]
+  \   /  
+   .-.   
+― (   ) ―
+   `-’   
+  /   \  
+
+[/yellow]"""
+        )
+    elif condition == weather_codes[1101] or condition == weather_codes[1102]:
+        print(
+            r"""[grey]
+
+                                
+        +++++++                 
+      +        +++++++         
+     +                 +       
+     +                 ++      
+   +++                 +++   
+ ++                       ++ 
+ +                         + 
+ ++                      ++ 
+   ++++++++++++++++++++++   
+                                
+
+[/grey]"""
+        )
+
+
 load_dotenv()
 
 TOMORROW_IO_KEY = getenv("TOMORROW_IO_API")
@@ -29,17 +62,7 @@ if response.status_code == 200:
     condition = weather_codes[values["weatherCode"]]
     print(f"The temperature in {location} is {values['temperature']}")
     print(f"It is {condition}.")
-    if condition == weather_codes[1000] or condition == weather_codes[1100]:
-        print(
-            r"""[yellow]
-  \   /  
-   .-.   
-― (   ) ―
-   `-’   
-  /   \  
-
-[/yellow]"""
-        )
+    display_weather_art()
 
 else:
     print(response)
